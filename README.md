8a9974a0934a5c7b16a0d1f0c28cca109f8b81d9
mukund1993 <mukundpatel1993@gmail.com>
2021-10-26 12:23:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-pdf-viewer-4zzfnt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-pdf-viewer-4zzfnt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+  textLayerRendered($event) {
+    console.log($event);
+  }
+}
